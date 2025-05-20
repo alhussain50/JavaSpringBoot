975993ca0a0d1dbd7f7f56d535254b88f8ca3544
Md. Masuk Al Hussain <masukalhussainwali.jatri@gmail.com>
2025-05-20 11:20:16 +0600

    Initial commit

---
+++ src/main/java/io/project/springbootstarter/SpringApiApp.java
@@ -0,0 +1,12 @@
+package io.project.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiApp.class, args);
+    }
+}
